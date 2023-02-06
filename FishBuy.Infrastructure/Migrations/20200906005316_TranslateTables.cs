77d00e6f104c03c0e4876081da489de75ddfd31c
Bruno Gorini <brunogorini@gmail.com>
2023-02-06 16:19:44 -0500

    Fix Migration

--- FishBuy.Infrastructure/Migrations/20200906005316_TranslateTables.cs
+++ FishBuy.Infrastructure/Migrations/20200906005316_TranslateTables.cs
@@ -142,138 +142,20 @@ namespace FishBuy.Infrastructure.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "OrderItems");
 
+            migrationBuilder.DropTable(
+                name: "Products");
 
-            migrationBuilder.CreateTable(
-                name: "FormaPagamento",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Descricao = table.Column<string>(type: "varchar(100) CHARACTER SET utf8mb4", maxLength: 100, nullable: false),
-                    Nome = table.Column<string>(type: "varchar(50) CHARACTER SET utf8mb4", maxLength: 50, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_FormaPagamento", x => x.Id);
-                });
+            migrationBuilder.DropTable(
+                name: "Orders");
 
-            migrationBuilder.CreateTable(
-                name: "Produtos",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Descricao = table.Column<string>(type: "varchar(400) CHARACTER SET utf8mb4", maxLength: 400, nullable: false),
-                    Nome = table.Column<string>(type: "varchar(50) CHARACTER SET utf8mb4", maxLength: 50, nullable: false),
-                    NomeArquivo = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
-                    Preco = table.Column<decimal>(type: "decimal(19,4)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Produtos", x => x.Id);
-                });
+            migrationBuilder.DropTable(
+                name: "PaymentMethods");
 
-            migrationBuilder.CreateTable(
-                name: "Usuarios",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Administrador = table.Column<bool>(type: "tinyint(1)", nullable: false),
-                    Email = table.Column<string>(type: "varchar(50) CHARACTER SET utf8mb4", maxLength: 50, nullable: false),
-                    Nome = table.Column<string>(type: "varchar(50) CHARACTER SET utf8mb4", maxLength: 50, nullable: false),
-                    Senha = table.Column<string>(type: "varchar(400) CHARACTER SET utf8mb4", maxLength: 400, nullable: false),
-                    SobreNome = table.Column<string>(type: "varchar(50) CHARACTER SET utf8mb4", maxLength: 50, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Usuarios", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Pedidos",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    CEP = table.Column<string>(type: "varchar(10) CHARACTER SET utf8mb4", maxLength: 10, nullable: false),
-                    Cidade = table.Column<string>(type: "varchar(100) CHARACTER SET utf8mb4", maxLength: 100, nullable: false),
-                    DataPedido = table.Column<DateTime>(type: "datetime(6)", nullable: false),
-                    DataPrevisaoEntrega = table.Column<DateTime>(type: "datetime(6)", nullable: false),
-                    EnderecoCompleto = table.Column<string>(type: "varchar(100) CHARACTER SET utf8mb4", maxLength: 100, nullable: false),
-                    Estado = table.Column<string>(type: "varchar(100) CHARACTER SET utf8mb4", maxLength: 100, nullable: false),
-                    FormaPagamentoId = table.Column<int>(type: "int", nullable: false),
-                    NumeroEndereco = table.Column<int>(type: "int", nullable: false),
-                    UsuarioId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Pedidos", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Pedidos_FormaPagamento_FormaPagamentoId",
-                        column: x => x.FormaPagamentoId,
-                        principalTable: "FormaPagamento",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Pedidos_Usuarios_UsuarioId",
-                        column: x => x.UsuarioId,
-                        principalTable: "Usuarios",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "ItemPedidos",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    PedidoId = table.Column<int>(type: "int", nullable: true),
-                    ProdutoId = table.Column<int>(type: "int", nullable: false),
-                    Quantidade = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ItemPedidos", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_ItemPedidos_Pedidos_PedidoId",
-                        column: x => x.PedidoId,
-                        principalTable: "Pedidos",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
-
-            migrationBuilder.InsertData(
-                table: "FormaPagamento",
-                columns: new[] { "Id", "Descricao", "Nome" },
-                values: new object[] { 1, "Forma de Pagamento Boleto", "Boleto" });
-
-            migrationBuilder.InsertData(
-                table: "FormaPagamento",
-                columns: new[] { "Id", "Descricao", "Nome" },
-                values: new object[] { 2, "Forma de Pagamento Cartão de Crédito", "Cartao de Crédito" });
-
-            migrationBuilder.InsertData(
-                table: "FormaPagamento",
-                columns: new[] { "Id", "Descricao", "Nome" },
-                values: new object[] { 3, "Forma de Pagamento Depósito", "Depósito" });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ItemPedidos_PedidoId",
-                table: "ItemPedidos",
-                column: "PedidoId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Pedidos_FormaPagamentoId",
-                table: "Pedidos",
-                column: "FormaPagamentoId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Pedidos_UsuarioId",
-                table: "Pedidos",
-                column: "UsuarioId");
+            migrationBuilder.DropTable(
+                name: "Users");           
         }
     }
 }
